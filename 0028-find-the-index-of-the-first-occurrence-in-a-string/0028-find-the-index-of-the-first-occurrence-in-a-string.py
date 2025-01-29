ebdd845204e8972a9949ea728ce3a13234e709a8
Alireza Shahbazpour <90491615+Dantedanette0@users.noreply.github.com>
2025-01-29 00:56:36 -0500

    Time: 0 ms (100%), Space: 17.6 MB (71.88%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
 
-        occur = 0
+
 
         if not needle:
             return -1
@@ -13,9 +13,6 @@ class Solution:
             return -1
 
 
-
-
-
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i+len(needle)] == needle:
                 return i
@@ -23,10 +20,9 @@ class Solution:
 
 
 
-        if occur == 0:
-            return -1
-        else:
-            return occur
+    
+        return -1
+ 
         
 
 
