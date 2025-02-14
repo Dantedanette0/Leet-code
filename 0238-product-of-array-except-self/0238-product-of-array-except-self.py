ce067d915ec3b44c87908170145a6ae0a2ba651e
Alireza Shahbazpour <90491615+Dantedanette0@users.noreply.github.com>
2025-02-13 19:44:28 -0500

    Time: 177 ms (5.08%), Space: 26.9 MB (19.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -8,14 +8,14 @@ class Solution:
 
 
             
-        # Calculate prefix products (inclusive of the current element)
+        
         prefix[0] = nums[0]
         for i in range(1, n):
             prefix[i] = prefix[i - 1] * nums[i]
 
-        # Calculate suffix products to match the desired output
-        suffix[-1] = nums[-1]  # Start with the last element included
-        for i in range(n - 2, -1, -1):  # Calculate suffix products moving backward
+        
+        suffix[-1] = nums[-1]  
+        for i in range(n - 2, -1, -1):  
             suffix[i] = suffix[i + 1] * nums[i]
         
         suffix.reverse()
