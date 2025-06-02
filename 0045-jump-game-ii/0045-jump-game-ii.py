f3599080a9691a8c0a7c4ad6bd653ba142862302
Alireza Shahbazpour <90491615+Dantedanette0@users.noreply.github.com>
2025-06-01 21:40:28 -0400

    Time: 4 ms (89.76%), Space: 18.5 MB (92.2%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,25 +1,21 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return 0
         
-        dest = len(nums) - 1
-        max_jumps = []
-        i = 0
-        while dest > 0:
-            if i > len(nums) - 1:
-                break
-
-            if nums[i] + i  >=  dest:
-               jump_amount =  nums[i]
-               if jump_amount + i >= dest:
-                    jump_amount = dest - i
-               print(nums[i]) 
-               print(f'jump, {jump_amount}')
-               max_jumps.append(i)
-               print(f'dest is, {dest}')
-               dest = dest - jump_amount 
-               i = 0
-            else:
-                i += 1
+        maxReach = 0
+        currReach = 0
+        jumps = 0
+        
+        for i in range(n - 1): 
+            maxReach = max(maxReach, i + nums[i])
             
-        print(max_jumps)
-        return len(max_jumps)
+            if i == currReach:
+                jumps += 1
+                currReach = maxReach
+                
+                if currReach >= n - 1:
+                    break
+                    
+        return jumps
